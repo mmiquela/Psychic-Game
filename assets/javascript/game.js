ecf36e655d85f2002c9689c57e4e878243c6fa38
Marsha Miquela <marshamiquela@Virgilios-MBP.home>
2017-11-18 19:25:45 -0500

    Fix comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,41 @@
-	
-	//Make our variables global to the runtime of our application
 	var ComputerGuess=['m','a','r','s','h','i','q','u','e','l'];
 	var wins =0;
 	var loss =0;
 	var retry =9;
 	var guess =[];
 	var answer=null;
-
-
-
 	var displayRetryCounter = function(){
 		document.querySelector('#Retry').innerHTML='Guesses Left: ' + retry;
 		document.querySelector('#Guesses').innerHTML='Guesses So far: ' + guess.join(', ');
 	};
-
 	var hideComputerGuess = function(){
 		this.answer = this.ComputerGuess[Math.floor(Math.random() * this.ComputerGuess.length)];
 		document.querySelector('#Retry').innerHTML='Guesses Left: ' + retry;
-
 	};
-
 	var displayInitial = function(){
 		retry=9;
 		guess=[];
 		hideComputerGuess();
 		displayRetryCounter();
 	}
-
 	hideComputerGuess();
-
 	var displayWins = function(){
 				wins++;
 				document.querySelector('#Wins').innerHTML='Wins: ' + wins;
 				alert("You guessed "  + answer + ". Well Done!!! Guess any key to play again.");
 				displayInitial();
 	};
-
 	var displayLosses = function(){
 				loss++;
 			document.querySelector('#Loss').innerHTML='Losses: ' + loss;
 			alert("Oh snap! Change a few things up and try submitting again.");
 			displayInitial();	
 	}
-
 	document.onkeyup = function(event){
 		retry--
 		var perguess = String.fromCharCode(event.keyCode).toLowerCase();
 		guess.push(perguess);
 		displayRetryCounter();
-
 		if (retry>0) {
 			if (perguess == answer) {
 				displayWins();
